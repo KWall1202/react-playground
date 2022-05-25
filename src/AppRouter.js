4e339ff9ca1fb78bbb4bbc5904cccd88793c916c
KWall1202 <kjwall97@yahoo.com>
2022-05-25 10:36:25 -0400

    Updated file structure for multiple pages, added basic NavBar and beginnings of page to interact with weather info

---
+++ src/AppRouter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import App from "./pages/App/App";
+import Weather from "./pages/Weather/Weather";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function AppRouter() {
+    return(
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route exact path='/' element={<App />} />
+                <Route path='/weather' element={<Weather />}  />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
---
+++ src/pages/Weather/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Weather() {
+    return (
+        <p>Here be the weather</p>
+    );
+}
+
+export default Weather;
