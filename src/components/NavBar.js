b96d050fca71ac279dfc2ab0343e0adf67f33744
KWall1202 <kjwall97@yahoo.com>
2022-05-25 10:52:04 -0400

    Updated Home link to properly link to home app page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/react-playground">Home</Link>
         </li>
         <li>
           <Link to="/weather">Weather</Link>
